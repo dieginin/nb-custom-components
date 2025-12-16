ccc1d2f7873e7ed5c786ffee40448e5b40fe0233
dieginin <diegobalestra9@gmail.com>
2025-12-16 15:35:39 -0800

    feat: add dist/ directory to .gitignore

--- dist/components/MyLabel.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./MyLabel.css";
-export const MyLabel = ({ allCaps = false, backgroundColor = "transparent", color, fontColor, label, size = "normal", }) => {
-    return (_jsx("span", { className: `${size} ${color} label`, style: { backgroundColor, color: `${fontColor}` }, children: allCaps ? label.toUpperCase() : label }));
-};
--- dist/stories/Page.d.ts
+++
@@ -1,3 +0,0 @@
-import "./page.css";
-import React from "react";
-export declare const Page: React.FC;
